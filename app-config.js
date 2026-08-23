4f3db3a4c39410b226b5fca5dd2ddecec15a1781
phattyfon-ai <phattyfon@gmail.com>
2026-08-23 21:34:40 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,14 +1,31 @@
 window.APP_CONFIG = {
 
+  /* =========================================================
+     LIFF
+  ========================================================= */
+
   LIFF_ID:
     "2011201679-uNWz5yqF",
+
+
+  /* =========================================================
+     API
+  ========================================================= */
 
   API: {
 
     DASHBOARD:
-      "https://nztvqdzatdpauufpvdaa.supabase.co/functions/v1/player-dashboard"
+      "https://nztvqdzatdpauufpvdaa.supabase.co/functions/v1/player-dashboard",
+
+    MISSION:
+      "https://nztvqdzatdpauufpvdaa.supabase.co/functions/v1/mission"
 
   },
+
+
+  /* =========================================================
+     PAGE
+  ========================================================= */
 
   PAGE: {
 
@@ -35,6 +52,11 @@ window.APP_CONFIG = {
 
   },
 
+
+  /* =========================================================
+     GAME SETTINGS
+  ========================================================= */
+
   EXP_PER_LEVEL:
     500,
 
@@ -45,11 +67,3 @@ window.APP_CONFIG = {
     30000
 
 };
-API: {
-
-  DASHBOARD:
-    "https://nztvqdzatdpauufpvdaa.supabase.co/functions/v1/player-dashboard",
-
-  MISSION:
-    "https://nztvqdzatdpauufpvdaa.supabase.co/functions/v1/mission"
-},
