ecab9208afcbfb639517298fd47528997fcb27d2
Rockyourbody2026 <phattyfon@gmail.com>
2026-08-27 20:19:02 +0700

    update app-config.js

---
+++ app-config.js
@@ -0,0 +1,28 @@
+/* ROCK YOUR BODY 2026 - SINGLE SOURCE CONFIG */
+window.APP_CONFIG = {
+  LIFF_ID: "2011201679-uNWz5yqF",
+  LIFF_URL: "https://liff.line.me/2011201679-uNWz5yqF",
+  ENDPOINT_URL: "https://rock-your-body.github.io/rock-your-body-2026/",
+  API: {
+    BASE: "https://rock-your-body-2026.onrender.com",
+    DASHBOARD: "https://rock-your-body-2026.onrender.com/api/player",
+    MISSION: "https://rock-your-body-2026.onrender.com/api/mission",
+    BATTLE: "https://rock-your-body-2026.onrender.com/api/battle",
+    MISSION_ADMIN: "https://rock-your-body-2026.onrender.com/api/mission"
+  },
+  PAGE: {
+    HOME: "./dashboard.html",
+    WEIGHT: "./weight-check.html",
+    PROGRESS: "./progress.html",
+    RANKING: "./ranking.html",
+    REWARDS: "./rewards.html",
+    MISSION: "./mission.html",
+    BATTLE: "./battle.html",
+    MISSION_ADMIN: "./mission-admin.html",
+    STEPS: "./steps.html",
+    FOOD: "./food-log.html"
+  },
+  MAX_ENERGY: 200,
+  EXP_PER_LEVEL: 500,
+  REFRESH_MS: 30000
+};
